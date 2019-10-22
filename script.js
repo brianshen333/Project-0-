cfc47fab5aeb9cb48e3e772edb9b8840f3abebdc
brianshen333 <34696040+brianshen333@users.noreply.github.com>
2019-10-21 22:07:11 -0400

    Add files via upload

--- script.js
+++ script.js
@@ -1,79 +1,54 @@
-import React from 'react';
-import { render } from 'react-dom';
-
-
 const classNames = {
   TODO_ITEM: 'todo-container',
   TODO_CHECKBOX: 'todo-checkbox',
   TODO_TEXT: 'todo-text',
   TODO_DELETE: 'todo-delete',
 }
-const Todo = props => (
-	<li>
-	<input type="checkbox" onClick={props.onToggle}/>
-	<button onClick = {props.onDelete}>delete</button>
-	<span>{props.text}</span>
-	</li>
-)
-var id = 0;
 
-class App extends React.Component {
-	constructor(){
-		super()
-		this.setState = {
-		 todos: [],
-		}
-	}
-	addTodo() {
-		const text = prompt("TODO text please!")
-		this.setState({
-		todos: [...this.state.todos, {id: id++,text: text},
-		],
-		})
-	}
+
+var form = document.getElementById('addForm');
+var itemList = document.getElementById('items');
+var check = document.getElementById('container');
+
+
+form.addEventListener('submit', addItem);
+form.addEventListener('submit', checkcount);
+itemList.addEventListener('click',removeItem);
+document.addEventListener('click',checkcount);
+
+function addItem(e){
+	e.preventDefault();
+	var newItem = document.getElementById('item').value;
+	var li = document.createElement('li');
+	var x = document.createElement("INPUT");
+	x.setAttribute("type", "checkbox");
+	li.className = 'list-group-item';
+	li.appendChild(document.createTextNode(newItem));
+	li.append(x);
+	var deleteButton = document.createElement('button');
+	deleteButton.appendChild(document.createTextNode('X'));
+	deleteButton.className = 'btn btn-danger btn-sm float-right delete';
+	li.appendChild(deleteButton);
+	itemList.appendChild(li);
 	
-	removeTodo(id) {
-	this.setState({
-		todos: this.state.todos.filter(todo => todo.id != id)
-		})
-	}
-	toggleTodo(id) {
-		this.setState({
-			todos: this.state.todos.map(todo => {
-				if (todo.id !== id) return todo 
-				return {
-					id:todo.id, 
-					text:todo.text,
-					checked: !todo.checked,
-				}
-			})
-		})
-	}
-	render(){
-	  <div class="container center">
-      <h1 class="center title">My TODO App</h1>
-      <div class="flow-right controls">
-        <span>Item count: <span id="item-count">{this.state.todos.length}</span></span>
-        <span>Unchecked count: <span id="unchecked-count">{this.state.todos.filter(todo => !todo.checked).length)}</span></span>
-      </div>
-	<button class="button center" onClick={() =>this.addTodo()}>New TODO</button>
-      <ul id="todo-list" class="todo-list">
-	  {this.state.todos.map(todo => <Todo onDelete={() => this.removeTodo(todo.id)}
-		onToggle={() => this.toggleTodo(todo.id)}
-		todo={todo}/>}
-	  </ul>
-    </div>
-	}
 }
-
-
-
-const list = document.getElementById('todo-list')
-const itemCountSpan = document.getElementById('item-count')
-const uncheckedCountSpan = document.getElementById('unchecked-count')
-
-function newTodo() {
-  alert('New TODO button clicked!')
+function removeItem(e){
+  if(e.target.classList.contains('delete')){
+    if(confirm('Are You Sure?')){
+      var li = e.target.parentElement;
+      itemList.removeChild(li);
+    }
+  }
 }
+function checkcount(e) {
 
-render(<App />, document.getElementById('root'))
+  var list = itemList.getElementsByTagName('li');
+  var listarray = Array.from(list);
+  
+  var total = listarray.length;
+  var countunchecked=listarray.filter( x => x.childNodes[1]["checked"] === false).length;
+  document.getElementById("item-count").innerHTML = total;
+  document.getElementById("unchecked-count").innerHTML = countunchecked;
+
+  }
+
